53f421bdc0e93cc490693b27be0c48b7140a19f7
Josh Smith <josh@xantie.com>
2023-04-28 11:13:07 -0600

    Initial Commit

---
+++ AppJack/AppJackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppJackApp.swift
+//  AppJack
+//
+//  Created by Josh Smith on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct AppJackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppJack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppJack
+//
+//  Created by Josh Smith on 4/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
